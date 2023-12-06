64596bd0e33ffd9abdde12b8308470c8022e63f0
Saloni <salonigupta12405@gmail.com>
2023-12-06 00:15:49 -0500

    initial commit

---
+++ Downloads/djangohw1/reviews/migrations/0001_initial.py
@@ -0,0 +1,19 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Review',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('review_text', models.CharField(blank=True, max_length=100)),
+            ],
+        ),
+    ]
